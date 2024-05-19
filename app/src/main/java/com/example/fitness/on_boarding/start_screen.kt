b3428c6d12004bbc47c1d452aa02fb623220474f
Ceme4k1n <117478188+Ceme4k1n@users.noreply.github.com>
2024-05-19 15:33:57 +0300

    Firs_Com

--- app/src/main/java/com/example/fitness/on_boarding/start_screen.kt
+++ app/src/main/java/com/example/fitness/on_boarding/start_screen.kt
@@ -1,5 +1,6 @@
 package com.example.fitness.on_boarding
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageButton
@@ -19,13 +20,34 @@ class start_screen : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start_screen)
 
+        //Кнопки скипа
+
+        val skip_button_text : TextView = findViewById(R.id.textskip_man_on_board_man_one)
+        val skip_button_svg : ImageView = findViewById(R.id.imageskip_arrow_svg)
+
+        //Первая панель
+
         val welcome_to_fitbody : ImageView = findViewById(R.id.imagewelcome_to_fitbody)
         val woman_on_board_back_one : ImageView = findViewById(R.id.imagewoman_on_board)
+
+        //Вторая панель
+
         val start_journey : ImageView = findViewById(R.id.imagepurple_start_journey)
         val man_on_board_two : ImageView = findViewById(R.id.imageman_on_board)
         val button_next_man_on_board_one : ImageView = findViewById(R.id.imageButton_next_on_man)
-        val skip_button_text : TextView = findViewById(R.id.textskip_man_on_board_man_one)
-        val skip_button_svg : ImageView = findViewById(R.id.imageskip_arrow_svg)
+
+        //Третья панель
+
+        val woman_on_board_three : ImageView = findViewById(R.id.imagewoman_on_board_three)
+        val button_next_woman_on_board_three : ImageView = findViewById(R.id.button_next_woman_on_board)
+        val find_nutrition_tips : ImageView = findViewById(R.id.image_find_nutrition_tips)
+
+        //Четвертая панель
+
+        val man_on_board_four : ImageView = findViewById(R.id.imageman_on_board_four)
+        val button_get_started : ImageView = findViewById(R.id.imageButton_get_started)
+        val a_community_for_you : ImageView = findViewById(R.id.imagecommunity_for_you)
+
 
 
         welcome_to_fitbody.visibility = View.VISIBLE
@@ -34,9 +56,6 @@ class start_screen : AppCompatActivity() {
 
         skip_button_svg.visibility = View.INVISIBLE
         skip_button_text.visibility = View.INVISIBLE
-        start_journey.visibility = View.INVISIBLE
-        man_on_board_two.visibility = View.INVISIBLE
-        button_next_man_on_board_one.visibility = View.INVISIBLE
 
 
 
@@ -56,14 +75,33 @@ class start_screen : AppCompatActivity() {
             start_journey.visibility = View.INVISIBLE
             man_on_board_two.visibility = View.INVISIBLE
             button_next_man_on_board_one.visibility = View.INVISIBLE
+
+            woman_on_board_three.visibility = View.VISIBLE
+            button_next_woman_on_board_three.visibility = View.VISIBLE
+            find_nutrition_tips.visibility = View.VISIBLE
+        }
+
+        button_next_woman_on_board_three.setOnClickListener {
+            woman_on_board_three.visibility = View.INVISIBLE
+            button_next_woman_on_board_three.visibility = View.INVISIBLE
+            find_nutrition_tips.visibility = View.INVISIBLE
+
+            man_on_board_four.visibility = View.VISIBLE
+            button_get_started.visibility = View.VISIBLE
+            a_community_for_you.visibility = View.VISIBLE
         }
 
         skip_button_text.setOnClickListener{
             start_journey.visibility = View.INVISIBLE
             man_on_board_two.visibility = View.INVISIBLE
             button_next_man_on_board_one.visibility = View.INVISIBLE
+            woman_on_board_three.visibility = View.INVISIBLE
+            button_next_woman_on_board_three.visibility = View.INVISIBLE
+            find_nutrition_tips.visibility = View.INVISIBLE
 
-
+            man_on_board_four.visibility = View.VISIBLE
+            button_get_started.visibility = View.VISIBLE
+            a_community_for_you.visibility = View.VISIBLE
         }
 
     }
