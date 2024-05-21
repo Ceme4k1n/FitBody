60a305aebd0813f416bca9ad565d3073994c80ed
Mnogochlenus <168830456+Mnogochlenus@users.noreply.github.com>
2024-05-21 21:09:39 +0300

    baza delay mnogo

--- app/src/main/java/com/example/fitness/on_boarding/login_database.kt
+++ app/src/main/java/com/example/fitness/on_boarding/login_database.kt
@@ -2,10 +2,27 @@ package com.example.fitness
 
 import android.app.Application
 import com.google.firebase.FirebaseApp
+import android.content.ContentValues
+import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.FirebaseAuthInvalidUserException
+
+
 
 class login_database : Application() {
     override fun onCreate() {
         super.onCreate()
         FirebaseApp.initializeApp(this)
+        fun regViaEmail(
+            email_base: String,
+            password_base: String
+        ){
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email_base,password_base,)
+                .addOnSuccessListener {
+                    Log.d(ContentValues.TAG, "Successfully registered a user")
+                }
+
+        }
     }
 }
